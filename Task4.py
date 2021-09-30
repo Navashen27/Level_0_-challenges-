c44836b222ef3e4b0de25532703ba75781a4596e
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2021-09-30 08:45:52 +0200

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,10 @@
+from typing import OrderedDict
+
+
+def add_even_or_odd (n2,n3):
+    result = n2_even + n3_odd 
+print("the sum of result ")
+
+number2 = 2
+number3 = 3 
+add_numbers (number2, number3)
