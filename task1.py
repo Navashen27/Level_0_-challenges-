f2d0e8fe78a6dfa572af30aa071753e6feed5831
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2021-10-06 12:12:37 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,12 @@
-print ("x=0")
-print ("y=1")
-print ("x=x+3")
-print ("y=y+x")
-print ("3")
-print ("1")
+#  pseudocode into actual code 
+x= (0)
+y= (1)
+
+print (x)
+print (y)
+
+x= (x + 3)
+y= (y + x)
+
+print (x)
+print (y)
