bfd40488a8119ff53d837a9d28900dc6570fb827
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2021-10-06 12:27:06 +0200

    Add files via upload

---
+++ Task 9.py
@@ -0,0 +1,7 @@
+# Vowels
+
+def vowel(text):
+    vowels = "aeiuoAEIOU"
+    print(len([letter for letter in text if letter in vowels]))
+    print([letter for letter in text if letter in vowels])
+vowel('Umuzi');
