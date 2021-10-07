8d87f2b5aab7669543a9d3b0e91b1894243b20ce
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2021-10-07 20:08:36 +0200

    Update Task 9.py

--- Task 9.py
+++ Task 9.py
@@ -1,7 +1,14 @@
-# Vowels
+# Python program to print vowels in a string
 
-def vowel(text):
-    vowels = "aeiuoAEIOU"
-    print(len([letter for letter in text if letter in vowels]))
-    print([letter for letter in text if letter in vowels])
-vowel('Umuzi');
+def printVowels(string):
+    # to print the vowels
+    for char in string:
+        if char in "aeiouAEIOU":
+            print(char, end=', ')
+    return char
+
+# take input
+string = input('Enter any string: ')
+
+# calling function
+printVowels(string)
