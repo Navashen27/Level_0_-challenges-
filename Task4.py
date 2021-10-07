6e90336bc2e5b51b5391347f36d3bab495e99394
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2021-10-07 12:50:15 +0200

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,10 +1,11 @@
-from typing import OrderedDict
-
-
-def add_even_or_odd (n2,n3):
-    result = n2_even + n3_odd 
-print("the sum of result ")
-
-number2 = 2
-number3 = 3 
-add_numbers (number2, number3)
+# Python function to check EVEN or ODD
+def CheckEvenOdd(num): 
+  if (num % 2 == 0): 
+    print(num," is EVEN")
+  else: 
+    print(num," is ODD")
+  
+CheckEvenOdd(11) 
+CheckEvenOdd(22) 
+CheckEvenOdd(33)
+CheckEvenOdd(44)
