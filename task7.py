d655f9bfac32734f28b06173b85b52d9bc613e38
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-01-20 09:08:09 +0200

    Update task7.py

--- task7.py
+++ task7.py
@@ -1,13 +1,12 @@
-# Funtion for Fahrenheit to Celsius and Vice Versa
-def convert_temp(scale=None, source_temp=None):
-    if scale == "F":
-        return 'C', (source_temp - 32.0) * (5.0/9.0)
-    elif scale == "C":
-        return 'F', (source_temp * (9.0/5.0)) + 32.0
-    else:
-        print("Needs to be (F) or (C)!")
 
-scale = input("Select (F) or (C): " )
-source_temp = int(input("What is the temperature: " ))
-s, m = convert_temp(scale, source_temp)
-print(source_temp, "degrees", scale, "is", m, "degrees", 
+# Temperature in celsius degree
+celsius = 40
+ 
+# Converting the temperature to
+# fehrenheit using the above
+# mentioned formula
+fahrenheit = (celsius * 1.8) + 32
+ 
+# printing the result
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+      %(celsius, fahrenheit))
