e9b83ea8f6861275c713b7c87970a65df2d7a028
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-01-20 09:40:36 +0200

    Update Task 9.py

--- Task 9.py
+++ Task 9.py
@@ -1,14 +1,8 @@
-# Python program to print vowels in a string
+vowels = ["a", "e", "i", "o", "u"]
+word = "pizza"
+empty_string = ""
+for letter in word:
+    if letter not in vowels:
+        empty_string += letter
 
-def printVowels(string):
-    # to print the vowels
-    for char in string:
-        if char in "aeiouAEIOU":
-            print(char, end=', ')
-    return char
-
-# take input
-string = input('Enter any string: ')
-
-# calling function
-printVowels(string)
+print(empty_string)
