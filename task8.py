febd4d95de14d10d3e803114a111358ca37c78a9
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-11 16:46:13 +0200

    Update task8.py

--- task8.py
+++ task8.py
@@ -1,10 +1,16 @@
-# Python Program to Convert seconds
-# into hours, minutes and seconds
-  
-def convertor(minutes):
-    print(minutes // 60)
-    print(minutes % 60)
-    
-minutes = int(input())
-convertor(minutes)
-
+def time_converter(time):
+    time = int(time)
+    hour = int(time / 60)
+    minutes = time % 60 
+    if time < 60 and minutes == 1:
+        print(minutes, "minute")
+    elif time < 60:
+        print(minutes, "minutes")
+    elif hour == 1 and minutes == 1:
+        print(hour, "hour,", minutes, "minute")
+    elif hour > 1 and minutes == 1:
+        print(hour, "hours,", minutes, "minute")
+    elif hour == 1:
+        print(hour, "hour,", minutes, "minutes")
+    else:
+        print(hour, "hours,", minutes, "minutes")
