cd439dbad7dd306d4e8bf38fea3f3bac176ae2f9
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-11 16:30:54 +0200

    Update task 5.py

--- task 5.py
+++ task 5.py
@@ -1,7 +1,9 @@
-#  Program to find the area of triangle
-a = 5
-b = 6
-c = 7
-s = (a + b + c) / 2
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+def area_of_triangle(x, y, z):
+    s = 0.5 * (x + y + z)
+    s1 = s - x
+    s2 = s - y
+    s3 = s - z
+    s4 = s1 * s2 * s3
+    pre_area = s * s4
+    area = sqrt(pre_area)
+    return area
