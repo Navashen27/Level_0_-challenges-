45057b22efa1bb6b139b02addc3d233e0c840992
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-11 16:49:05 +0200

    Update Task 9.py

--- Task 9.py
+++ Task 9.py
@@ -1,8 +1,17 @@
-vowels = ["a", "e", "i", "o", "u"]
-word = "pizza"
-empty_string = ""
-for letter in word:
-    if letter not in vowels:
-        empty_string += letter
+def isVowel_upper(c):
+    if c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U':
+        return c
 
-print(empty_string)
+def isVowel_lower(c):
+    if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
+        return c
+
+def vowels_checker(str):
+    vowels = []
+    s = ''
+    i = 0
+    while i < len(str):
+        if str[i] == isVowel_upper(str[i]) or str[i] == isVowel_lower(str[i]):
+            vowels.append(str[i])
+        i = i + 1
+    print(s.join(vowels))
