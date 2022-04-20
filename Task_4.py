2d199da38627c99891d874358e391d1f441b3783
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-20 16:11:52 +0200

    Update and rename Task4.py to Task_4.py

---
+++ Task_4.py
@@ -0,0 +1,12 @@
+# Python function to examine even_or_odd numbers
+
+def   even_or_odd(num): 
+  if (num % 2 == 0): 
+    print(num," is EVEN")
+  else: 
+    print(num," is ODD")
+  
+even_or_odd(11) 
+even_or_odd(22) 
+even_or_odd(33)
+even_or_odd(44)
