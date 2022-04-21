c41e2d42854803e99ac8e88944abb44bfd76baf3
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-21 09:40:27 +0200

    Update and rename task6.py to task_6.py

--- task6.py
+++
@@ -1,8 +0,0 @@
-def maximum(a, b, c):
-    list = [a, b, c]
-    return max(list)
-  
-a = 10
-b = 14
-c = 12
-print(maximum(a, b, c))
---
+++ task_6.py
@@ -0,0 +1,13 @@
+def maximum_num(*numbers):
+    
+    biggest = numbers[0]
+
+    for num in numbers:
+
+        if num > biggest:
+
+            biggest = num
+
+    return biggest
+
+print(maximum_num(10, 1, 105))
