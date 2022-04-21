1072f0170192321c3c3e4e7f0c210d639b8f8303
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-21 09:18:05 +0200

    Update and rename Task 9.py to Task _9.py

---
+++ Task _9.py
@@ -0,0 +1,11 @@
+def vowel_extract(data):
+    data = data.lower()
+    vowels = [ 'a','e','i','o','u']
+    vowels_in_data = []
+    for  letter in data:
+        if letter in vowels:
+            vowels_in_data.append(letter)
+    print("Vowels: "+", " . join(set(vowels_in_data)) )
+
+vowel_extract ("Umuzi")
+    #check in data if there is vowels
