d3ea77ac2b64e4185f9c3d6cf32f5a0a58c77cb8
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-04-21 09:27:04 +0200

    Update and rename Task 10.py to Task _10.py

--- Task _10.py
+++ Task _10.py
@@ -1,7 +1,7 @@
 def common_letters(string1, string2):
     string1 = string1.lower()
     string2 = string2.lower()
-    output = " ".join(set(string1).intersection(string2))
+    output = " " + "," . join(set(string1).intersection(string2))
     return output
 
 x = common_letters("Miracles", "messages")
