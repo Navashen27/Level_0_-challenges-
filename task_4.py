ba2a67a824a6e3dab51ea8c415549969efd78fc8
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-05-24 13:04:47 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,11 +1,7 @@
+def even_or_odd(num):
+    if num % 2 == 0:
+        print('even')
+    else:
+        print('odd')
 
-def   even_or_odd(num): 
-  if (num % 2 == 0): 
-    print(num," is EVEN")
-  else: 
-    print(num," is ODD")
-  
-even_or_odd(11) 
-even_or_odd(22) 
-even_or_odd(33)
-even_or_odd(44)
+even_or_odd(2)
