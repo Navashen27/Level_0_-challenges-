a1db8c7d59630436fffc8ea5ec00f4ae9c6725e7
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-06-21 19:16:42 +0200

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,8 +1,4 @@
-def common_letters(string1, string2):
-    string1 = string1.lower()
-    string2 = string2.lower()
-    output = " " + "," . join(set(string1).intersection(string2))
-    return output
-
-x = common_letters("Miracles", "messages")
-print(f"Common letters: {x}")
+def commonletter(firstword,secondword):
+   words=set(firstword.lower()) & set(secondword.lower())
+   print(*{letter for letter in words},sep=(', '))
+commonletter("miracles","messages")
