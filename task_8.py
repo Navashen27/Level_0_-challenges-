2a62eac6a5da1cbed0e09a848f5b6ab80a5185ec
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-06-21 19:23:19 +0200

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -3,12 +3,12 @@ def time_conv(time):
     hours = time // 60
     minutes = time % 60
     if hours == 1 and minutes == 1:
-        print (f"{hours}) hour, {minutes} minute")
+        print (f" {hours} hour,{minutes} minute")
     elif hours == 1 and minutes >= 0:
-        print (f"{hours}) hour, {minutes} minutes")
+        print (f" {hours} hour,{minutes} minutes")
     elif hours >=0 and minutes ==1:
-        print (f"{hours}) hour, {minutes} minute")
+        print (f"{hours} hour,{minutes} minute")
     else:
-        print (f"{hours}) hours, {minutes} minutes")
+        print (f"{hours} hours,{minutes} minutes")
 
 time_conv(100)
