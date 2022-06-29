c3c7413d6b77f1ebcc929f9824e2e0283b88fae0
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-06-29 14:29:18 +0200

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,4 +1,4 @@
-def commonletter(firstword,secondword):
-   words=set(firstword.lower()) & set(secondword.lower())
-   print(*{letter for letter in words},sep=(', '))
-commonletter("miracles","messages")
+def common_letter(firstword,secondword):
+       words=set(firstword.lower()) & set(secondword.lower())
+       print(*{letter for letter in words},sep=(', '))
+common_letter("miracles","messages")
