8ae990cedb93569050ee0d925fad1ae93e5ca229
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-06-30 09:45:51 +0200

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,4 +1,19 @@
-def common_letter(firstword,secondword):
-       words=set(firstword.lower()) & set(secondword.lower())
-       print(*{letter for letter in words},sep=(', '))
-common_letter("miracles","messages")
+def common_letters(first_string, second_string):
+    print("common letters: ", end="")
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    remover = "%s"
+    output = ""
+    
+    for letters in first_string:
+        for characters in second_string:
+                if letters == characters:
+                    if letters not in output:
+                        output = output + letters
+                        print(remover % letters, end = "")
+                        remover = ", %s"
+                else:
+                    continue
+        
+
+common_letters("houses","computers")
