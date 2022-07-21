efb0e4f2ac1b91908655b625c6ecb7ba74b73a91
Navashen27 <91491408+Navashen27@users.noreply.github.com>
2022-07-21 22:09:13 +0200

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,8 +1,10 @@
-def common(input1, input2):
-    word1 = set(input1.lower())
-    word2 = set(input2.lower())
-    characters = (", ").join(word1 & word2)
+def compare(string_1, string_2):
+    empty = 'Common letters: '
+    string_1 = string_1.lower()
+    string_2 = string_2.lower()
+    for char in string_1:
+        if char in string_2:
+            empty += f'{char}, '
+    print(empty[:-2])
 
-    print("Common letters: {}".format(characters))
-
-common("house", "cOmputers")
+compare('hOuse', 'ComputEr')
